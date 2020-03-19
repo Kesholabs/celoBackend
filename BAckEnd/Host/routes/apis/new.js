4a2c8d65f7b3c7a70ffd76f0b8af7f570b1ea769
grandmullah <collinsbiegon9@gmail.com>
2020-03-19 08:57:41 +0300

    bugger in account registraion

--- BAckEnd/Host/routes/apis/new.js
+++ BAckEnd/Host/routes/apis/new.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const blockServiceController = require('../../controllers/apis/version1/blockservice')
-const  authClient = require('../../middleware/authenticationgaurd')
 const userController = require('../../controllers/apis/version1/user.controller') 
 let router = express.Router();
 
--- BAckEnd/Host/services/version1/user/user.js
+++ BAckEnd/Host/services/version1/user/user.js
@@ -1,41 +1,29 @@
 const express = require('express')
+const web3 = require("web3");
+const fs = require("fs");
+const web3Instance = new web3();
 
 const register = async (req,res,next) => {
-    let errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(422).json({ errors: errors.array() });
-    }
-    let {name,email,password} = req.body
-    let emailExists = await userModel.findOne({"Email":email})
-
-    if(emailExists){
-        return res.status(500).json({
-            Message:"failed Email already exists"
-        })
-    }
-    let hashedPassword = await bcrypt.hash(password, 8);
+   const identity = req.body.phoneNumber
     try {
-        let wallet = new ethers.Wallet.createRandom()
-        let encryptPromise = wallet.encrypt(password)
-        encryptPromise.then(function(json) {
-            let user =  userModel.create({
-                Name:name,
-                Email:email,
-                Password:hashedPassword,
-                Wallet:json               
-            })
-            if(!user){
+        console.log("Creating a new account");
+        const account = web3Instance.eth.accounts.create();
+        console.log(`Made new account ${account.address}`);
+        fs.writeFileSync(identity, account.privateKey);
+        console.log(`Account private key saved to ${identity}`);
+        
+            if(!account){
                 throw new error()
             }
             res.status(200).json({
                 Message:"success",
                 details:{
-                    "name":name,
-                    "Email":email
+                    "message":"success",
+                    "Account address":account.address
                 }
             })
             
-        })
+       
 
        
         
@@ -51,52 +39,9 @@ const register = async (req,res,next) => {
     }
 }
 
-const login = async (req,res,next) => {
 
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        return res.status(422).json({ errors: errors.array() });
-    }
-
-    let { email, password } = req.body
-
-    try{
-        let isUserExists = await userModel.findOne({"Email" : email});
-        // console.log(isUserExists.Password)
-        let isPasswordValid = await bcrypt.compare(password, isUserExists.Password);
-
-        if(!isUserExists || !isPasswordValid){
-            return res.status(401).json({
-                "errors" : [{
-                    "msg" : "email/password is wrong"
-                }]
-            })
-        }
-
-        let token = jwt.sign({ id: isUserExists._id }, config.secret, { expiresIn: 86400 });
-
-        res.status(200).json({
-            "success" : [{
-                "msg" : "user login successfully",
-                "email" : email,
-                "token" : token
-            }]
-        })
-    }catch(error){
-        console.log(error);
-        return res.status(500).json(
-            { 
-                "errors" : [{
-                    "msg": "there was a problem login a user."   
-                }]
-            }
-        );
-    }
-    
-}
 
 module.exports = ({
     register:register,
-    login:login
+    
 })
